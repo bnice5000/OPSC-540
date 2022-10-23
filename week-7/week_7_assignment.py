74d5391dafd0dcac64bba58c4f23a326f5ea6d85
Brian Levin <brian4lawschool@gmail.com>
2022-10-23 19:22:53 -0400

    Finished week 7 assignment

--- week-7/week_7_assignment.py
+++ week-7/week_7_assignment.py
@@ -19,6 +19,14 @@ import sqlalchemy as db
 
 
 def read_file(filename):
+    """Reads in a file
+
+    Args:
+        filename (string): the name of the file to be read into the application
+
+    Returns:
+        list[string]: a list of the lines of the file in string format
+    """
 
     with open(filename, "r", encoding="utf-8") as file:
         lines = file.readlines()
@@ -27,6 +35,12 @@ def read_file(filename):
 
 
 def write_database(hacks):
+    """Write information to database
+
+    Args:
+        hacks (list[{type: type of hack, instance: example of the hack}]): a list of dictionary of string
+    """
+
     engine = db.create_engine("sqlite:///hacks.sqlite")
     connection = engine.connect()
     metadata = db.MetaData()
@@ -39,28 +53,38 @@ def write_database(hacks):
     )
     metadata.create_all(engine)
     query = db.insert(hack_table)
-    ReultsProxy = connection.execute(query, hacks)
+    results = connection.execute(query, hacks)
+    results.close()
+    connection.close()
+    engine.dispose()
 
 
-log_file = read_file("modsec_audit.log.1")
-hack_list = []
+def main():
+    """the main function that controls the application
+    """
+    log_file = read_file("modsec_audit.log.1")
+    hack_list = []
 
-for idx, line in enumerate(log_file):
-    if "-B--" in line:
-        potential_hack = log_file[(idx + 1) % len(log_file)]
-        if "../" in potential_hack:
-            hack_list.append({"type": "traversal", "instance": potential_hack})
-        elif "rfi" in potential_hack:
-            hack_list.append(
-                {"type": "Remote File Inclusion", "instance": potential_hack}
-            )
-        elif "DBMS" in potential_hack:
-            hack_list.append({"type": "SQL Injection", "instance": potential_hack})
-        elif ".htaccess" in potential_hack:
-            hack_list.append(
-                {"type": "htaccess accessible", "instance": potential_hack}
-            )
-        else:
-            hack_list.append({"type": "Unidentified", "instance": potential_hack})
+    for idx, line in enumerate(log_file):
+        if "-B--" in line:
+            potential_hack = log_file[(idx + 1) % len(log_file)]
+            if "../" in potential_hack:
+                hack_list.append({"type": "traversal", "instance": potential_hack})
+            elif "rfi" in potential_hack:
+                hack_list.append(
+                    {"type": "Remote File Inclusion", "instance": potential_hack}
+                )
+            elif "DBMS" in potential_hack:
+                hack_list.append({"type": "SQL Injection", "instance": potential_hack})
+            elif ".htaccess" in potential_hack:
+                hack_list.append(
+                    {"type": "htaccess accessible", "instance": potential_hack}
+                )
+            else:
+                hack_list.append({"type": "Unidentified", "instance": potential_hack})
 
-write_database(hack_list)
+    write_database(hack_list)
+
+if __name__ == "__main__":
+    main()
+    # end main
